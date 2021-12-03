c5aba83de56656ba7e04b999594b2cc6c1fc361d
Donghee Lee <mechanicdong95@gmail.com>
2021-12-03 23:14:46 +0900

    Initial Commit

---
+++ QuotesGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuotesGenerator
+//
+//  Created by 이동희 on 2021/12/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
