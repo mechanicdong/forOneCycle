e23523436f14dd7a06dfcd80c287578f25fe0edf
Donghee Lee <mechanicdong95@gmail.com>
2021-12-08 23:24:56 +0900

    화면간 데이터 전달, Navigation Controller 익숙해지기, 화면 전환 방법 활용

--- LEDBoard/ViewController.swift
+++ LEDBoard/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LEDBoardSettingDelegate {
 
+    @IBOutlet weak var contentsLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.contentsLabel.textColor = .yellow
     }
-
-
+    //segueway로 화면전환을 구현했기 때문에 prepare 메서드 사용
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let settingViewController = segue.destination as? SettingViewController {
+            settingViewController.delegate = self
+            settingViewController.ledText = self.contentsLabel.text
+            settingViewController.textColor = self.contentsLabel.textColor
+            settingViewController.backgroundColor = self.view.backgroundColor ?? .black
+        }
+    }
+    func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor) {
+        if let text = text {
+            self.contentsLabel.text  = text
+        }
+        self.contentsLabel.textColor = textColor
+        self.view.backgroundColor = backgroundColor
+    }
 }
 
