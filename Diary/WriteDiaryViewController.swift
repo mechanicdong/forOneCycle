2a37a7efdd7b85d97d741730c4c1c13268478cca
Donghee Lee <ehdgml9419@gmail.com>
2021-12-26 22:07:50 +0900

    일기작성 화면의 UI 구성 @20211226LDH

---
+++ Diary/WriteDiaryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WriteDiaryViewController.swift
+//  Diary
+//
+//  Created by 이동희 on 2021/12/26.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
