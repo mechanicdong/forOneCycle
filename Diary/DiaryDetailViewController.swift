0d0ac87b2212fffe58ee7ef6061a86c2f082c5e2
Donghee Lee <ehdgml9419@gmail.com>
2022-01-03 23:56:31 +0900

    일기장 목록에서 일기장 상세항목 이동 구현 @20220103LDH

--- Diary/DiaryDetailViewController.swift
+++ Diary/DiaryDetailViewController.swift
@@ -9,8 +9,36 @@ import UIKit
 
 class DiaryDetailViewController: UIViewController {
 
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var contentsTextView: UITextView!
+    @IBOutlet var dateLabel: UILabel!
+    
+    var diary: Diary?
+    var indexPath: IndexPath?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureView()
+    }
+    
+    private func configureView() {
+        guard let diary = self.diary else { return }
+        self.titleLabel.text = diary.title
+        self.contentsTextView.text = diary.contents
+        self.dateLabel.text = self.dateToString(date: diary.date)
+    }
+    
+    //date type으로 전달받으면 문자열로 변환하는 메소드 생성
+    private func dateToString(date : Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yy년 MM월 dd일(EEEEE)"
+        formatter.locale = Locale(identifier: "ko_KR")
+        return formatter.string(from: date)
     }
 
+    @IBAction func tabEditButton(_ sender: UIButton) {
+    }
+    @IBAction func tabDeleteButton(_ sender: UIButton) {
+    }
 }
