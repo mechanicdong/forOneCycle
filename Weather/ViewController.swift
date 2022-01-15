53f7d3aac86c8e2bae9f280d9fcef114af1d9ed2
Donghee Lee <ehdgml9419@gmail.com>
2022-01-15 22:30:29 +0900

    weatherInformation 객체를 사용하여 UI에 정보 표시 20220115@LDH

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet var tempLabel: UILabel!
     @IBOutlet var maxTempLabel: UILabel!
     @IBOutlet var minTempLabel: UILabel!
+    @IBOutlet var weatherStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,15 +28,29 @@ class ViewController: UIViewController {
             self.view.endEditing(true) //button이 눌리면 키보드가 사라지게
         }
     }
+    func configureView(weatherInformation: WeatherInformation) {
+        self.cityNameLabel.text = weatherInformation.name
+        if let weather = weatherInformation.weather.first {
+            self.weatherDescriptionLabel.text = weather.description
+        }
+        self.tempLabel.text = "\(Int(weatherInformation.temp.temp - 273.15))도"
+        self.minTempLabel.text = "최저: \(Int(weatherInformation.temp.minTemp - 273.15))"
+        self.maxTempLabel.text = "최고: \(Int(weatherInformation.temp.maxTemp - 273.15))"
+    }
     
     func getCurrentWeather(cityName: String) {
-        guard let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=\(cityName) &appid=6ef24838143eb1b4d16156344f13bfec") else { return }
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=6ef24838143eb1b4d16156344f13bfec") else { return }
         let session = URLSession(configuration: .default)
-        session.dataTask(with: url) { data, response, error in
+        session.dataTask(with: url) { [weak self] data, response, error in
             //JSON Data는 data 파라미터로 전달
             guard let data = data, error == nil else { return } //정상일 때 error는 nil 반환
             let decoder = JSONDecoder() //JSON 객체에서 데이터 유형으로 디코딩하는 객체
-            let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data)
+            guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data) else { return }
+            DispatchQueue.main.async {
+                //main Thread
+                self?.weatherStackView.isHidden = false
+                self?.configureView(weatherInformation: weatherInformation)
+            }
         }.resume()
     }
 }
