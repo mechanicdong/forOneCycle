3384bbd38f8fe172967c54f2dc060f384c1bfcb7
Donghee Lee <ehdgml9419@gmail.com>
2022-01-15 22:41:38 +0900

    존재하지 않는 도시를 검색할 경우 알람 메시지 추가

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -38,19 +38,35 @@ class ViewController: UIViewController {
         self.maxTempLabel.text = "최고: \(Int(weatherInformation.temp.maxTemp - 273.15))"
     }
     
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "에러", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     func getCurrentWeather(cityName: String) {
         guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=6ef24838143eb1b4d16156344f13bfec") else { return }
         let session = URLSession(configuration: .default)
         session.dataTask(with: url) { [weak self] data, response, error in
+            let successRange = (200..<300) //200~299 성공범위
             //JSON Data는 data 파라미터로 전달
             guard let data = data, error == nil else { return } //정상일 때 error는 nil 반환
             let decoder = JSONDecoder() //JSON 객체에서 데이터 유형으로 디코딩하는 객체
-            guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data) else { return }
-            DispatchQueue.main.async {
-                //main Thread
-                self?.weatherStackView.isHidden = false
-                self?.configureView(weatherInformation: weatherInformation)
+            if let response = response as? HTTPURLResponse, successRange.contains(response.statusCode) {
+                //만약 전달받은 코드가 200번대라면
+                guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data) else { return }
+                DispatchQueue.main.async {
+                    //main Thread
+                    self?.weatherStackView.isHidden = false
+                    self?.configureView(weatherInformation: weatherInformation)
+                }
+            } else {
+                guard let errorMessage = try? decoder.decode(ErrorMessage.self, from: data) else { return }
+                DispatchQueue.main.async {
+                    self?.showAlert(message: errorMessage.message)
+                }
             }
+
         }.resume()
     }
 }
