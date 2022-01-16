3f60f9a1f0a58ffac6287a5ca228f00160ac61b6
Donghee Lee <ehdgml9419@gmail.com>
2022-01-16 16:07:37 +0900

    Initial Commit

---
+++ COVID19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by 이동희 on 2022/01/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
