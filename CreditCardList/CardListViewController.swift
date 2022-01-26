4de6fe4fd42d7d8e046ae6eed48de6ed4f808afc
Donghee Lee <ehdgml9419@gmail.com>
2022-01-26 21:26:36 +0900

    using Lottie & Kingfisher open source 20220126@LDH

--- CreditCardList/CardListViewController.swift
+++ CreditCardList/CardListViewController.swift
@@ -6,7 +6,48 @@
 //
 
 import UIKit
- 
+import Kingfisher
+
 class CardViewListController: UITableViewController {
+    //전달받은 데이터의 가공형태를 지정
+    var creditCardList: [CreditCard] = []
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        //UITableView Cell Register
+        let nibName = UINib(nibName: "CardListCell", bundle: nil)
+        tableView.register(nibName, forCellReuseIdentifier: "CardListCell")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return creditCardList.count
+    }
+    
+    //custom cell과 그 cell에 전달된 데이터 지정
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CardListCell", for: indexPath) as? CardListCell else { return UITableViewCell() } //없다면 일반적인 UITableViewCell을 반환
+        cell.rankLabel.text = "\(creditCardList[indexPath.row].rank)위"
+        cell.promotionLabel.text = "\(creditCardList[indexPath.row].promotionDetail.amount)만원 증정"
+        cell.cardNameLabel.text = "\(creditCardList[indexPath.row].name)"
+        
+        //Image를 로컬에 저장하지 않고 URL로 불러오기(Kingfisher Opensource)
+        let imageURL = URL(string: creditCardList[indexPath.row].cardImageURL)
+        cell.cardImageView.kf.setImage(with: imageURL)
+
+        return cell
+    }
+    
+    //cell의 높이 지정
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        //상세화면전달
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        guard let detailViewController = storyboard.instantiateViewController(identifier: "CardDetailViewController") as? CardDetailViewController else { return }
+        detailViewController.promotionDetail = creditCardList[indexPath.row].promotionDetail  
+    }
 }
