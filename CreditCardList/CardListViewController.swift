b5c37050e7cd1efbf22838c50745790a29b7a688
Donghee Lee <ehdgml9419@gmail.com>
2022-01-31 22:38:27 +0900

    RealTime DB 연결하여 데이터 불러오기 20220131@LDH

--- CreditCardList/CardListViewController.swift
+++ CreditCardList/CardListViewController.swift
@@ -7,18 +7,44 @@
 
 import UIKit
 import Kingfisher
+import Firebase
 
 class CardViewListController: UITableViewController {
+    var ref: DatabaseReference! //Firebase Realtime Database를 가져올 수 있는 reference 값
+    
     //전달받은 데이터의 가공형태를 지정
     var creditCardList: [CreditCard] = []
-    
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //UITableView Cell Register
         let nibName = UINib(nibName: "CardListCell", bundle: nil)
         tableView.register(nibName, forCellReuseIdentifier: "CardListCell")
+        
+        self.ref = Database.database().reference()
+        
+        //Realtime db는 snapshot을 이용해서 data를 불러옴
+        self.ref.observe(.value) { snapshot in
+            guard let value = snapshot.value as? [String: [String: Any]] else { return }
+            //JSON Decoding
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: value)
+                let cardData = try JSONDecoder().decode([String: CreditCard].self, from: jsonData)
+                let cardList = Array(cardData.values)
+                self.creditCardList = cardList.sorted {
+                    //1위부터 10위까지 순차 표기
+                    $0.rank < $1.rank
+                }
+                //tableview reload -> UI 움직임 -> Main Thread에서 제공됨 -> 클로저안에서 메인에서 돌도록 지정
+                //main thread에 해당 action 넣기
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            } catch let error {
+                print("ERROR JSON parsing \(error.localizedDescription)")
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -28,14 +54,14 @@ class CardViewListController: UITableViewController {
     //custom cell과 그 cell에 전달된 데이터 지정
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "CardListCell", for: indexPath) as? CardListCell else { return UITableViewCell() } //없다면 일반적인 UITableViewCell을 반환
-        cell.rankLabel.text = "\(creditCardList[indexPath.row].rank)위"
-        cell.promotionLabel.text = "\(creditCardList[indexPath.row].promotionDetail.amount)만원 증정"
-        cell.cardNameLabel.text = "\(creditCardList[indexPath.row].name)"
         
         //Image를 로컬에 저장하지 않고 URL로 불러오기(Kingfisher Opensource)
         let imageURL = URL(string: creditCardList[indexPath.row].cardImageURL)
         cell.cardImageView.kf.setImage(with: imageURL)
-
+        cell.rankLabel.text = "\(creditCardList[indexPath.row].rank)위"
+        cell.promotionLabel.text = "\(creditCardList[indexPath.row].promotionDetail.amount)만원 증정"
+        cell.cardNameLabel.text = creditCardList[indexPath.row].name
+    
         return cell
     }
     
@@ -44,10 +70,11 @@ class CardViewListController: UITableViewController {
         return 80
     }
     
-    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //상세화면전달
-        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
         guard let detailViewController = storyboard.instantiateViewController(identifier: "CardDetailViewController") as? CardDetailViewController else { return }
-        detailViewController.promotionDetail = creditCardList[indexPath.row].promotionDetail  
+        detailViewController.promotionDetail = creditCardList[indexPath.row].promotionDetail
+        self.show(detailViewController, sender: nil)
     }
 }
