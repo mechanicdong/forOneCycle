de4f3804b6b9b549126a329dbc07016f9153c072
Donghee Lee <ehdgml9419@gmail.com>
2022-02-13 21:15:38 +0900

    dev initial & navigation rootViewController only using Code for SnapKit lib. 20220213@LDH

---
+++ NetFlixStyleApp/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  NetFlixStyleApp
+//
+//  Created by 이동희 on 2022/02/13.
+//
+
+//Snapkit을 사용하여 storyboard 없이 Code로 구현
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+    
+}
