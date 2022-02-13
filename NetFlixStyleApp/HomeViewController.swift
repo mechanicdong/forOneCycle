90f8212c2930a32a6b2a42fe98dbb7e4a98b465e
Donghee Lee <ehdgml9419@gmail.com>
2022-02-13 21:32:39 +0900

    set navigationBar & etc button 20220213@LDH

--- NetFlixStyleApp/HomeViewController.swift
+++ NetFlixStyleApp/HomeViewController.swift
@@ -11,4 +11,17 @@ import UIKit
 
 class HomeViewController: UICollectionViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Set Navigation
+        navigationController?.navigationBar.backgroundColor = .clear
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.hidesBarsOnSwipe = true
+        
+        //add button
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage.init(imageLiteralResourceName: "netflix_icon"), style: .plain, target: nil, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.crop.circle"), style: .plain, target: nil, action: nil)
+    }
 }
