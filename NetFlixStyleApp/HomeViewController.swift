8607e67ee3b9b9eed1b9acf587b2ae5440ccd7e8
Donghee Lee <ehdgml9419@gmail.com>
2022-02-18 22:40:39 +0900

    Set Large Size Section Layout when emphasizing Netflix series 20220218@LDH

--- NetFlixStyleApp/HomeViewController.swift
+++ NetFlixStyleApp/HomeViewController.swift
@@ -18,7 +18,7 @@ class HomeViewController: UICollectionViewController {
         super.viewDidLoad()
         
         //Set Navigation
-        navigationController?.navigationBar.backgroundColor = .clear
+        navigationController?.navigationBar.backgroundColor = .black
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
         navigationController?.hidesBarsOnSwipe = true
@@ -55,11 +55,13 @@ class HomeViewController: UICollectionViewController {
     private func layout() -> UICollectionViewLayout {
         return UICollectionViewCompositionalLayout { [weak self] sectionNumber, environment -> NSCollectionLayoutSection? in
             
-            guard let self = self else { return nil}
+            guard let self = self else { return nil }
             
             switch self.contents[sectionNumber].sectionType {
             case .basic:
                 return self.createBasicTypeSection()
+            case .large:
+                return self.createLargeTypeSection()
             default:
                 return nil
             }
@@ -85,6 +87,27 @@ class HomeViewController: UICollectionViewController {
         return section
     }
     
+    //Set Large Size Section Layout
+    private func createLargeTypeSection() -> NSCollectionLayoutSection {
+        //item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3), heightDimension: .fractionalHeight(0.75))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 10, leading: 5, bottom: 0, trailing: 5)
+        //group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9), heightDimension: .estimated(400))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        //section
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        
+        //set header
+        let sectionHeader = self.createSectionHeader()
+        section.boundarySupplementaryItems = [sectionHeader]
+        section.contentInsets = .init(top: 0, leading: 5, bottom: 0, trailing: 5)
+        
+        return section
+    }
+    
     //Set SectionHeader Layout
     private func createSectionHeader() -> NSCollectionLayoutBoundarySupplementaryItem {
         //Section Header Size
@@ -101,7 +124,7 @@ class HomeViewController: UICollectionViewController {
 extension HomeViewController {
     //Section당 보여지는 Cell의 개수
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if contents[section].sectionType == .basic {
+        if contents[section].sectionType == .basic || contents[section].sectionType == .large {
             switch section {
             case 0:
                 return 1
@@ -109,7 +132,7 @@ extension HomeViewController {
                 return contents[section].contentItem.count
             }
         }
-        return 0        
+        return 0
     }
     
     //Set CollectionView Cell
@@ -150,19 +173,19 @@ extension HomeViewController {
 }
 
 //SwiftUI를 활용한 미리보기
-//struct HomeViewController_Previews: PreviewProvider {
-//    static var previews: some View {
-//        Container().edgesIgnoringSafeArea(.all)
-//    }
-//
-//    struct Container: UIViewControllerRepresentable {
-//        func makeUIViewController(context: Context) -> UIViewController {
-//            let layout = UICollectionViewFlowLayout()
-//            let homeViewController = HomeViewController(collectionViewLayout: layout)
-//            return UINavigationController(rootViewController: homeViewController)
-//        }
-//        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
-//        typealias UIViewControllerType = UIViewController
-//    }
-//}
+struct HomeViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        Container().edgesIgnoringSafeArea(.all)
+    }
+
+    struct Container: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> UIViewController {
+            let layout = UICollectionViewFlowLayout()
+            let homeViewController = HomeViewController(collectionViewLayout: layout)
+            return UINavigationController(rootViewController: homeViewController)
+        }
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+        typealias UIViewControllerType = UIViewController
+    }
+}
 
