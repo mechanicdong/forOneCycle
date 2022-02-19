ffbaaddca46cd97152c85382a817cfa90add6d45
Donghee Lee <ehdgml9419@gmail.com>
2022-02-19 22:23:40 +0900

    set menustackview button components action  20220219@LDH

--- NetFlixStyleApp/ContentCollectionViewMainCell.swift
+++ NetFlixStyleApp/ContentCollectionViewMainCell.swift
@@ -12,7 +12,10 @@ class ContentCollectionViewMainCell: UICollectionViewCell {
     let baseStackView = UIStackView()
     let menuStackView = UIStackView()
     
-    
+    //menuStackView Components
+    let tvButton = UIButton()
+    let movieButton = UIButton()
+    let categoryButton = UIButton()
     
     override func layoutSubviews() {
         super.layoutSubviews()
@@ -38,9 +41,38 @@ class ContentCollectionViewMainCell: UICollectionViewCell {
         menuStackView.distribution = .equalSpacing
         menuStackView.spacing = 20
         
+        [tvButton, movieButton, categoryButton].forEach {
+            menuStackView.addArrangedSubview($0)
+            $0.setTitleColor(.white, for: .normal)
+            $0.layer.shadowColor = UIColor.black.cgColor //shadowColor는 CGColor를 상속받고 있음
+            $0.layer.shadowOpacity = 1
+            $0.layer.shadowRadius = 3
+        }
+        
+        tvButton.setTitle("TV 프로그램", for: .normal)
+        movieButton.setTitle("영화", for: .normal)
+        categoryButton.setTitle("카테고리 ▼", for: .normal)
+        
+        //menuStackView button IBAction
+        tvButton.addTarget(self, action: #selector(tvButtonTapped), for: .touchUpInside)
+        movieButton.addTarget(self, action: #selector(movieButtonTapped), for: .touchUpInside)
+        categoryButton.addTarget(self, action: #selector(categoryButtonTapped), for: .touchUpInside)
+        
         menuStackView.snp.makeConstraints {
             $0.top.equalTo(baseStackView)
             $0.leading.trailing.equalToSuperview().inset(30)
         }
+    }
+    
+    @objc func tvButtonTapped(sender: UIButton!) {
+        print("TEST: TV Button Tapped")
+    }
+    
+    @objc func movieButtonTapped(sender: UIButton!) {
+        print("TEST: Movie Button Tapped")
+    }
+    
+    @objc func categoryButtonTapped(sender: UIButton!) {
+        print("TEST: Category Button Tapped")
     }
 }
