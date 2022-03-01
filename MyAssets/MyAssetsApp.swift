8e26f779c8a1d9d88b4dea55dd8c6dc302cb63a0
Donghee Lee <ehdgml9419@gmail.com>
2022-03-01 20:54:48 +0900

    Initial Commit

---
+++ MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by 이동희 on 2022/03/01.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
