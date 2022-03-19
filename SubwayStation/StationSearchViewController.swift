6e9a01660ea31e0c33cefe60e0c8092081444e33
Donghee Lee <ehdgml9419@gmail.com>
2022-03-19 20:12:36 +0900

    set TableViewLayout

--- SubwayStation/StationSearchViewController.swift
+++ SubwayStation/StationSearchViewController.swift
@@ -9,20 +9,52 @@ import UIKit
 import SnapKit
 
 class StationSearchViewController: UIViewController {
-
+    //create tableview
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setNavigationItems()
+        setTableViewLayout()
+    }
+    
+    private func setNavigationItems() {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "지하철 도착 정보"
         
         let searchController = UISearchController()
         searchController.searchBar.placeholder = "지하철역을 입력해주세요"
-        searchController.obscuresBackgroundDuringPresentation = false // TODO
+        searchController.obscuresBackgroundDuringPresentation = false // 색상 반투명 해제
         
         navigationItem.searchController = searchController
     }
+    
+    //set layout
+    private func setTableViewLayout() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
 
-
+extension StationSearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //이번에는 tableviewcell을 custom 하지 않으므로 바로 초기화해서 사용
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath.item)"
+        
+        return cell
+    }
 }
 
