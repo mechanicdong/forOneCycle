742db2c2b18fdd6e79b3f9cf14660c7fd6541fbc
Donghee Lee <ehdgml9419@gmail.com>
2022-03-23 16:14:24 +0900

    UIButton Customization as UIButton+

--- Instagram/Scenes/FeedTableViewCell.swift
+++ Instagram/Scenes/FeedTableViewCell.swift
@@ -8,19 +8,6 @@
 import UIKit
 import SnapKit
 
-extension UIButton {
-    //좋아요 버튼의 크기가 commentButton, DMButton과 맞지 않아 조정
-    func setImage(systemName: String) {
-        contentHorizontalAlignment = .fill
-        contentVerticalAlignment = .fill
-        
-        imageView?.contentMode = .scaleAspectFit
-        //imageEdgeInsets = .zero //deprecated in iOS 15.0
-        
-        setImage(UIImage(systemName: systemName), for: .normal)
-    }
-}
-
 final class FeedTableViewCell: UITableViewCell {
     
     private lazy var postImageView: UIImageView = {
