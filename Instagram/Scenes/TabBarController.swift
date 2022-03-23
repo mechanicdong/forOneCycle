5461c8e62b1bf07fe87040b9673bb0db0b84c148
Donghee Lee <ehdgml9419@gmail.com>
2022-03-23 15:43:14 +0900

    set FeedTableViewCell layout

---
+++ Instagram/Scenes/TabBarController.swift
@@ -0,0 +1,24 @@
+//
+//  TabBarController.swift
+//  Instagram
+//
+//  Created by 이동희 on 2022/03/23.
+//
+
+import UIKit
+import SnapKit
+
+final class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //UINavigationVC에 임베디드
+        let feedViewController = UINavigationController(rootViewController: FeedViewController())
+        feedViewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
+        
+        let profileViewController = UIViewController()
+        profileViewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person.fill "))
+        
+        viewControllers = [feedViewController, profileViewController]
+    }
+}
