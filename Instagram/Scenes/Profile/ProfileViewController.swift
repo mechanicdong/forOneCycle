29126d66475b779605f6ab6016dc119ad627295a
Donghee Lee <ehdgml9419@gmail.com>
2022-03-24 16:30:05 +0900

    add Profile Collection View Cell

--- Instagram/Scenes/Profile/ProfileViewController.swift
+++ Instagram/Scenes/Profile/ProfileViewController.swift
@@ -64,16 +64,50 @@ final class ProfileViewController: UIViewController {
     private let followerDataView = ProfileDataView(title: "팔로워", count: 11111)
     private let followingDataView = ProfileDataView(title: "팔로잉", count: 59)
     
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 0.5
+        layout.minimumInteritemSpacing = 0.5
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(ProfileCollectionViewCell.self, forCellWithReuseIdentifier: "ProfileCollectionViewCell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavigationBar()
         setupLayout()
     }
-    
 }
 
-extension ProfileViewController {
+extension ProfileViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProfileCollectionViewCell", for: indexPath) as? ProfileCollectionViewCell
+        cell?.setup(with: UIImage())
+        
+        return cell ?? UICollectionViewCell()
+    }
+}
+
+//collection view 간격 설정
+extension ProfileViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = (collectionView.frame.width / 3) - 1.0
+        return CGSize(width: width, height: width)
+    }
+}
+
+private extension ProfileViewController {
     
     func setupNavigationBar() {
         navigationItem.title = "UserName"
@@ -104,6 +138,7 @@ extension ProfileViewController {
             nameLabel,
             descriptionLabel,
             buttonStackView,
+            collectionView
         ].forEach {
             view.addSubview($0)
         }
@@ -139,6 +174,12 @@ extension ProfileViewController {
             $0.top.equalTo(descriptionLabel.snp.bottom).offset(12.0)
             $0.leading.equalTo(nameLabel.snp.leading)
             $0.trailing.equalTo(nameLabel.snp.trailing)
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(buttonStackView.snp.bottom).offset(16.0)
+            $0.bottom.equalToSuperview()
         }
         
     }
