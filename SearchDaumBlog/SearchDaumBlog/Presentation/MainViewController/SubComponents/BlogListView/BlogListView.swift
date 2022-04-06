10c33cd84a7d85617d0b3c2d652e865452f47902
Donghee Lee <ehdgml9419@gmail.com>
2022-04-06 22:18:22 +0900

    dev Blog List View

---
+++ SearchDaumBlog/SearchDaumBlog/Presentation/MainViewController/SubComponents/BlogListView/BlogListView.swift
@@ -0,0 +1,60 @@
+//
+//  BlogListView.swift
+//  SearchDaumBlog
+//
+//  Created by 이동희 on 2022/04/06.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+class BlogListView: UITableView {
+    let disposeBag = DisposeBag()
+    
+    let headerView = FilterView(
+        frame: CGRect(
+            origin: .zero,
+            size: CGSize(width: UIScreen.main.bounds.width, height: 50)
+        )
+    )
+    
+    //MainVC에서 작업한 네트워크 값을 -> BlogListView,Cell 등
+    let cellData = PublishSubject<[BlogListCellData]>()
+    
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+        
+        bind()
+        attribute()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //cellForRowAt 함수를 Rx로 표현하면 다음과 같다!
+    private func bind() {
+        cellData
+            .asDriver(onErrorJustReturn: [])
+            .drive(self.rx.items) { tableview, row, data in
+                let index = IndexPath(row: row, section: 0)
+                let cell = tableview.dequeueReusableCell(withIdentifier: "BlogLisgCell", for: index) as! BlogListCell
+                cell.setData(data)
+                
+                return cell
+            }
+            .disposed(by: disposeBag)
+        
+        
+    }
+    
+    private func attribute() {
+        self.backgroundColor = .white
+        self.register(BlogListCell.self, forCellReuseIdentifier: "BlogListCell")
+        self.separatorStyle = .singleLine
+        self.rowHeight = 100
+        self.tableHeaderView = headerView
+    }
+}
