f3da11f3aad7771209449c10d9dcc5c226a86917
Donghee Lee <ehdgml9419@gmail.com>
2022-04-07 21:22:20 +0900

    API Network

---
+++ SearchDaumBlog/SearchDaumBlog/Network/SearchBlogNetwork.swift
@@ -0,0 +1,49 @@
+//
+//  SearchBlogNetwork.swift
+//  SearchDaumBlog
+//
+//  Created by 이동희 on 2022/04/06.
+//
+
+import Foundation
+import RxSwift
+
+
+enum SearchNetworkError: Error {
+    case invalidURL
+    case invalidJSON
+    case networkError
+}
+
+class SearchBlogNetwork {
+    private let session: URLSession
+    let api = SearchBlogAPI()
+    
+    init(session: URLSession = .shared) {
+        self .session = session
+    }
+    
+    func searchBlog(query: String) -> Single<Result<DKBlog, SearchNetworkError>> {
+        guard let url = api.searchBlog(query: query).url else {
+            return .just(.failure(.invalidURL))
+        }
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("KakaoAK 711021ccb7d353191643aaa977f72244", forHTTPHeaderField: "Authorization")
+        
+        return session.rx.data(request: request as URLRequest)
+            .map { data in
+                do {
+                    let blogData = try JSONDecoder().decode(DKBlog.self, from: data)
+                    return .success(blogData)
+                } catch {
+                    return .failure(.invalidJSON)
+                }
+            }
+            .catch { _ in
+                    .just(.failure(.networkError))
+            }
+            .asSingle()
+    }
+}
